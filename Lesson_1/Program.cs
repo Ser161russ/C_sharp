e9744d6ae36744dc022d74b295e7c600d9b17b82
sergey <kozhokar.s@mail.ru>
2023-06-22 21:06:02 +0300

    classwork 1_0

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишит пррамму кторая на вход принмает число и выдает его квадрат число умноженое само на себя 
+
+
+Console.WriteLine("Write a number:");
+string num_str = Console.ReadLine()!;
+
+// int num = int.Parse(num_str);
+int num = int.Parse(num_str);
+Console.WriteLine (num*num);
